8617e0d580019b3e7243d24d679fcb97b2d6361e
rockie2695 <21993381+rockie2695@users.noreply.github.com>
2022-01-05 17:08:02 +0800

    init crud-example

---
+++ resources/views/index.blade.php
@@ -0,0 +1,47 @@
+@extends('layout')
+
+@section('content')
+<style>
+    .uper {
+        margin-top: 40px;
+    }
+</style>
+<div class="uper">
+    @if(session()->get('success'))
+    <div class="alert alert-success">
+        {{ session()->get('success') }}
+    </div><br />
+    @endif
+    <h1 class="dislay-1">Corona virus records in the wrold</h1>
+    <a class="btn btn-primary" href="{{ route('coronas.create') }}" role="button" style="margin-bottom:10px">Add</a>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <td>ID</td>
+                <td>Country Name</td>
+                <td>Symptoms</td>
+                <td>Cases</td>
+                <td colspan="2">Action</td>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($coronacases as $case)
+            <tr>
+                <td>{{$case->id}}</td>
+                <td>{{$case->country_name}}</td>
+                <td>{{$case->symptoms}}</td>
+                <td>{{$case->cases}}</td>
+                <td><a href="{{ route('coronas.edit', $case->id)}}" class="btn btn-primary">Edit</a></td>
+                <td>
+                    <form action="{{ route('coronas.destroy', $case->id)}}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-danger" type="submit">Delete</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    <div>
+        @endsection
